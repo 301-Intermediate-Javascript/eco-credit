eeccc51c96ef1f584fd467cfb0201e556dade2aa
ashleybiermann <ashleybiermann@gmail.com>
2020-05-26 15:50:51 -0700

    created schema tables and added three fakeusers

--- data/schema.sql
+++ data/schema.sql
@@ -1,12 +1,41 @@
 
-DROP TABLE books;
+DROP TABLE IF EXISTS profiles CASCADE;
+DROP TABLE IF EXISTS location CASCADE;
+DROP TABLE IF EXISTS surveyinfo CASCADE;
+DROP TABLE IF EXISTS carboninfo CASCADE;
 
-CREATE TABLE books (
+CREATE TABLE profiles (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255),
-  author VARCHAR(255),
-  isbn VARCHAR(255),
-  description TEXT,
-  image VARCHAR(255),
-  bookshelf INTEGER
-)
+  username VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE location (
+  id SERIAL PRIMARY KEY,
+  username INT NOT NULL,
+  zipcode INT,
+  FOREIGN KEY (username) REFERENCES profiles (id)
+);
+
+CREATE TABLE surveyinfo (
+  id SERIAL PRIMARY KEY,
+  username INT NOT NULL,
+  ecoscore INT,
+  energy FLOAT,
+  shower INT,
+  car_travel FLOAT,
+  FOREIGN KEY (username) REFERENCES profiles (id)
+);
+
+CREATE TABLE carboninfo (
+  id SERIAL PRIMARY KEY,
+  username INT NOT NULL,
+  ecoscore INT,
+  energy FLOAT,
+  shower FLOAT,
+  car_travel FLOAT,
+  FOREIGN KEY (username) REFERENCES profiles (id)
+);
+
+SELECT * FROM location JOIN profiles ON location.username = profiles.id;
+SELECT * FROM surveyinfo JOIN profiles ON surveyinfo.username = profiles.id;
+SELECT * FROM carboninfo JOIN profiles ON carboninfo.username = profiles.id;
